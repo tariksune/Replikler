64c273cb4556275af93b599355059020bdd362cd
tariksune <tariksune@gmail.com>
2020-10-26 02:46:39 +0300

    developing

--- app/src/main/java/com/tarxsoft/replikler/SplashActivity.java
+++ app/src/main/java/com/tarxsoft/replikler/SplashActivity.java
@@ -3,12 +3,15 @@ package com.tarxsoft.replikler;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
 
 public class SplashActivity extends AppCompatActivity {
 
     private static int SPLASH_TIME_OUT = 2000;
+    SharedPreferences onboardingScreen;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,9 +23,22 @@ public class SplashActivity extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent loginIntent = new Intent(SplashActivity.this,OnboardingActivity.class);
-                startActivity(loginIntent);
-                finish();
+
+                onboardingScreen = getSharedPreferences("onboardingScreen",MODE_PRIVATE);
+                boolean isNewUser = onboardingScreen.getBoolean("newUser", true);
+                if (isNewUser){
+                    SharedPreferences.Editor editor = onboardingScreen.edit();
+                    editor.putBoolean("newUser",false);
+                    editor.commit();
+                    Intent loginIntent = new Intent(SplashActivity.this,OnboardingActivity.class);
+                    startActivity(loginIntent);
+                    finish();
+                }else{
+                    Intent mainIntent = new Intent(SplashActivity.this,MainActivity.class);
+                    startActivity(mainIntent);
+                    finish();
+                }
+
             }
         },SPLASH_TIME_OUT);
     }
